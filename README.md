6dc6c40e19200646714d3beb2a631391419fa613
siosw-specular <simon@specular.network>
2024-11-15 15:10:59 +0100

    initial example

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fabriq SDK Example
+
+A minimal example script that bridges an ERC20 from Sepolia to OP Sepolia by publishing an intent to our public intent pool. Our solver will then try to fill the intent.
+
+## Setup
+
+1. Currently our solver only operates on a small set of custom test tokens. Message us on [Discord](https://discord.com/invite/gacndQvTpP) and we will send you some.
+2. Once you have an account with some test tokens, add its private key to the script. You will also need a small amount of gas.
+
+## Running the exampe
+
+```
+npm install
+npm run start
+```
---
+++ index.ts
@@ -0,0 +1,50 @@
+import { IntentClient, getSupportedChainByID, getTestnetTokensByChainID, createItem } from '@fabriqnetwork/sdk';
+import { createWalletClient, Hex, http, WalletClient, parseUnits } from 'viem';
+import { optimismSepolia, sepolia } from 'viem/chains';
+import { privateKeyToAccount } from 'viem/accounts';
+
+// reach out on discord to get some testnet tokens
+const privateKey = 'ADD YOUR PRIVATE KEY HERE' as Hex
+const intentPoolURL = 'io.fabriq.sh:9997';
+
+async function main() {
+	const walletClientSepolia: WalletClient = createWalletClient({
+		account: privateKeyToAccount(privateKey),
+		chain: sepolia,
+		transport: http(),
+	});
+
+	// get fabriq contract deployments for sepolia 
+	const sepoliaInfo = await getSupportedChainByID(sepolia.id);
+
+	// @ts-expect-error "viem WalletClient re-export error"
+	const intentClientSepolia = new IntentClient(walletClientSepolia, intentPoolURL, sepoliaInfo);
+
+	const tokensSepolia = await getTestnetTokensByChainID(sepolia.id);
+	const tokensOptimismSepolia = await getTestnetTokensByChainID(optimismSepolia.id);
+
+	// fabriq uses permit2 for gasless token alowances
+	// if the permit2 contract is already approved, this does not do anything
+	await intentClientSepolia.approvePermit2(tokensSepolia.Ceres);
+
+	// the solver will only solve profitable orders, that's why the output is lower than the input
+	const inAmount = parseUnits('2.00', 18);
+	const outAmount = parseUnits('1.90', 18);
+
+	// submitting an intent to bridge Ceres from Sepolia to OP Sepolia 
+	// the 2 amounts in each Item specify a dutch auction
+	// by setting them to the same value we create a static price
+	const input = createItem(tokensSepolia.Ceres, inAmount, inAmount, sepolia.id);
+	const output = createItem(tokensOptimismSepolia.Ceres, outAmount, outAmount, optimismSepolia.id)
+	// intents have can multiple outputs, in that case exactly one output will be filled 
+	// outputs can be on the same or on different chains
+	await intentClientSepolia.submitIntent(input, [output]);
+}
+
+main()
+	.then(() => {
+		console.log('successfully submitted intent');
+	})
+	.catch((error) => {
+		console.error('Error:', error);
+	});
